65d73f15882df15ce2d2306c970ec8c961d003f3
LCmry <lcrees.2014@gmail.com>
2016-02-20 13:04:11 -0800

    Added individual blog post view and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
 from blog import views
 
-urlpatterns = [url(r'^$', views.index, name='index'),]
+urlpatterns = [url(r'^$', views.index, name='index'),
+              url(r'^(?P<post_id>\d+)/$', views.post, name='post'),
+              ]
