459ef7447c584e0876d562f738d01b42b1ac2a7c
LCmry <lcrees.2014@gmail.com>
2016-02-20 13:54:06 -0800

    Friendlier urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),
-              url(r'^(?P<post_id>\d+)/$', views.post, name='post'),
+              url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
               ]
