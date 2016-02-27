b88ae1ef62de1b3da6b1ad890d76f4edf525a845
LCmry <lcrees.2014@gmail.com>
2016-02-27 11:58:45 -0800

    Added add_post to views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.template import Context, loader
+from django.shortcuts import render, get_object_or_404, render_to_response, redirect
+from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
 
 from blog.models import Post
+from blog.forms import PostForm
 
 def index(request):
   latest_posts = Post.objects.all().order_by('-created_at')
@@ -20,3 +21,16 @@ def post(request, post_url):
   t = loader.get_template('blog/post.html')
   c = Context({'single_post': single_post, })
   return HttpResponse(t.render(c))
+
+def add_post(request):
+  context = RequestContext(request)
+  if request.method == 'POST':
+    form = PostForm(request.POST, request.FILES)
+    if form.is_valid():
+      form.save(commit=True)
+      return redirect(index)
+    else:
+      print form.errors
+  else:
+    form = PostForm()
+    return render_to_response('blog/add_post.html', {'form': form}, context)
